4a0fc12a6e7658834f9243e071c838614070de49
Huân Lê-Vương <65440815+lehuan5062@users.noreply.github.com>
2025-11-04 08:00:30 +0700

    Revert "Make animation callback methods public in MonsterCharacter (#6)"
    This reverts commit fb68ade8cbbd2428b501c47b2b9b111d719a0f7a.

--- Source/AuraMonster/Public/MonsterCharacter.h
+++ Source/AuraMonster/Public/MonsterCharacter.h
@@ -55,21 +55,6 @@ public:
 	UFUNCTION(BlueprintCallable, Category = "Monster|Crawling")
 	void UpdateSurfaceAttachment(float DeltaTime);
 
-	/** Called when a subtle neck twitch should be animated */
-	UFUNCTION(BlueprintNativeEvent, Category = "Monster|Idle")
-	void OnNeckTwitch();
-	virtual void OnNeckTwitch_Implementation();
-
-	/** Called when a subtle finger shift should be animated */
-	UFUNCTION(BlueprintNativeEvent, Category = "Monster|Idle")
-	void OnFingerShift();
-	virtual void OnFingerShift_Implementation();
-
-	/** Called to update breathing animation intensity (0.0 to 1.0) */
-	UFUNCTION(BlueprintNativeEvent, Category = "Monster|Idle")
-	void OnBreathingUpdate(float BreathingIntensity);
-	virtual void OnBreathingUpdate_Implementation(float BreathingIntensity);
-
 protected:
 	/** Current behavior state of the monster */
 	UPROPERTY(VisibleAnywhere, BlueprintReadOnly, Category = "Monster")
@@ -111,6 +96,21 @@ protected:
 	UFUNCTION(BlueprintNativeEvent, Category = "Monster")
 	void OnBehaviorStateChanged(EMonsterBehaviorState OldState, EMonsterBehaviorState NewState);
 	virtual void OnBehaviorStateChanged_Implementation(EMonsterBehaviorState OldState, EMonsterBehaviorState NewState);
+
+	/** Called when a subtle neck twitch should be animated */
+	UFUNCTION(BlueprintNativeEvent, Category = "Monster|Idle")
+	void OnNeckTwitch();
+	virtual void OnNeckTwitch_Implementation();
+
+	/** Called when a subtle finger shift should be animated */
+	UFUNCTION(BlueprintNativeEvent, Category = "Monster|Idle")
+	void OnFingerShift();
+	virtual void OnFingerShift_Implementation();
+
+	/** Called to update breathing animation intensity (0.0 to 1.0) */
+	UFUNCTION(BlueprintNativeEvent, Category = "Monster|Idle")
+	void OnBreathingUpdate(float BreathingIntensity);
+	virtual void OnBreathingUpdate_Implementation(float BreathingIntensity);
 
 	/** Called when the monster transitions to a new surface */
 	UFUNCTION(BlueprintNativeEvent, Category = "Monster|Crawling")
